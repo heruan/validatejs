2fbd7ec2acc72d1a7bc1a692444f3732ca31df93
Giovanni Lovato <giovanni.lovato@saiv.it>
2016-08-10 10:40:04 +0200

    feat(rule-dependency): add rule dependency support

--- src/validation-rules.js
+++ src/validation-rules.js
@@ -22,59 +22,64 @@ export class ValidationRules {
     throw new Error('not implemented');
   }
 
-  addRule(key, rule) {
-    this.rules.push({ key: key, rule: rule });
+  addRule(key, rule, dependsOn) {
+    this.rules.push({ key: key, rule: rule, dependsOn: dependsOn });
   }
   ensure(prop) {
     this.currentProperty = prop;
     return this;
   }
-  length(configuration) {
-    this.addRule(this.currentProperty, ValidationRule.lengthRule(configuration));
+  length(configuration, dependsOn?) {
+    this.addRule(this.currentProperty, ValidationRule.lengthRule(configuration), dependsOn);
     return this;
   }
-  presence(configuration) {
-    this.addRule(this.currentProperty, ValidationRule.presence(configuration));
+  presence(configuration, dependsOn?) {
+    this.addRule(this.currentProperty, ValidationRule.presence(configuration), dependsOn);
     return this;
   }
-  required(configuration) {
-    this.addRule(this.currentProperty, ValidationRule.presence(configuration));
+  required(configuration, dependsOn?) {
+    this.addRule(this.currentProperty, ValidationRule.presence(configuration), dependsOn);
     return this;
   }
-  numericality(configuration) {
-    this.addRule(this.currentProperty, ValidationRule.numericality(configuration));
+  numericality(configuration, dependsOn?) {
+    this.addRule(this.currentProperty, ValidationRule.numericality(configuration), dependsOn);
     return this;
   }
-  date(configuration) {
-    this.addRule(this.currentProperty, ValidationRule.date(configuration));
+  date(configuration, dependsOn?) {
+    this.addRule(this.currentProperty, ValidationRule.date(configuration), dependsOn);
     return this;
   }
-  datetime(configuration) {
-    this.addRule(this.currentProperty, ValidationRule.datetime(configuration));
+  datetime(configuration, dependsOn?) {
+    this.addRule(this.currentProperty, ValidationRule.datetime(configuration), dependsOn);
     return this;
   }
-  email(configuration) {
-    this.addRule(this.currentProperty, ValidationRule.email(configuration));
+  email(configuration, dependsOn?) {
+    this.addRule(this.currentProperty, ValidationRule.email(configuration), dependsOn);
     return this;
   }
-  equality(configuration) {
-    this.addRule(this.currentProperty, ValidationRule.equality(configuration));
+  equality(configuration, dependsOn?) {
+    if (!dependsOn && typeof configuration === "string") {
+      dependsOn = [ configuration ];
+    } else if (!dependsOn && typeof configuration === "function") {
+      dependsOn = [ configuration() ];
+    }
+    this.addRule(this.currentProperty, ValidationRule.equality(configuration), dependsOn);
     return this;
   }
-  format(configuration) {
-    this.addRule(this.currentProperty, ValidationRule.format(configuration));
+  format(configuration, dependsOn?) {
+    this.addRule(this.currentProperty, ValidationRule.format(configuration), dependsOn);
     return this;
   }
-  inclusion(configuration) {
-    this.addRule(this.currentProperty, ValidationRule.inclusion(configuration));
+  inclusion(configuration, dependsOn?) {
+    this.addRule(this.currentProperty, ValidationRule.inclusion(configuration), dependsOn);
     return this;
   }
-  exclusion(configuration) {
-    this.addRule(this.currentProperty, ValidationRule.exclusion(configuration));
+  exclusion(configuration, dependsOn?) {
+    this.addRule(this.currentProperty, ValidationRule.exclusion(configuration), dependsOn);
     return this;
   }
-  url(configuration) {
-    this.addRule(this.currentProperty, ValidationRule.url(configuration));
+  url(configuration, dependsOn?) {
+    this.addRule(this.currentProperty, ValidationRule.url(configuration), dependsOn);
     return this;
   }
 }
